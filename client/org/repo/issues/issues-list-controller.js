3f38fc8cd1522df9d3e9978126461b4b91593f38
Jeff Cross <middlefloor@gmail.com>
2015-01-22 10:51:43 -0800

    WIP: implement cache updating mechanism

--- client/org/repo/issues/issues-list-controller.js
+++ client/org/repo/issues/issues-list-controller.js
@@ -1,20 +1,25 @@
 (function() {
 
 function IssuesListController ($http, $scope, $routeParams, db,
-    lovefieldQueryBuilder, mockIssues, firebaseAuth) {
+    issuesCacheUpdater, lovefieldQueryBuilder, mockIssues, firebaseAuth) {
   var ITEMS_PER_PAGE = 30;
   var observeQuery;
-  var queryStartTime = performance.now();
   var table = db.getSchema().getIssues();
+  var predicate = lovefieldQueryBuilder({
+    repository: $routeParams.repo,
+    organization: $routeParams.org
+  });
 
   $scope.issues = [];
   $scope.$on('$locationChangeStart', updateQueryAndSubscription);
 
   getAndRenderIssues(db).
     then(countPages).
-    then(subscribeToIssues);//.
-    // then(buildUpdateUrl);//.
-    // then(updateIssuesCache);
+    then(updateCount).
+    then(subscribeToIssues).
+    then(function() {
+      issuesCacheUpdater(db);
+    });
 
   $scope.$on('$destroy', unobserve);
 
@@ -27,16 +32,16 @@ function IssuesListController ($http, $scope, $routeParams, db,
   }
 
   function getStandardQuery() {
-    var predicate = lovefieldQueryBuilder({
-      repository: $routeParams.repo,
-      organization: $routeParams.org
-    });
-    var pageNum = angular.isDefined($routeParams.page) ? parseInt($routeParams.page, 10) : 0;
+    var pageNum = 0;
+
+    if(angular.isDefined($routeParams.page)) {
+      pageNum = parseInt($routeParams.page, 10);
+    }
 
     //Get initial value
 
     var query = db.
-      select().
+      select(table.number, table.id, table.title).
       from(table).
       limit(ITEMS_PER_PAGE);
 
@@ -52,10 +57,6 @@ function IssuesListController ($http, $scope, $routeParams, db,
   }
 
   function getCountQuery() {
-    var predicate = lovefieldQueryBuilder({
-      repository: $routeParams.repo,
-      organization: $routeParams.org
-    });
     return db.
       select(lf.fn.count(table.id)).
       from(table).
@@ -70,20 +71,19 @@ function IssuesListController ($http, $scope, $routeParams, db,
     return Promise.resolve(db);
   }
 
-  function countPages(db) {
-    getCountQuery().
-      exec().
-      then(function(count) {
-        var pages = Math.ceil(count[0]['COUNT(id)'] / ITEMS_PER_PAGE);
-        $scope.$apply(function(){
-          $scope.pages=new Array(pages);
-        });
-      });
-
-    return db;
+  function countPages() {
+    return getCountQuery().
+      exec();
   }
 
-  function getAndRenderIssues(db) {
+  function updateCount(count) {
+    var pages = Math.ceil(count[0]['COUNT(id)'] / ITEMS_PER_PAGE);
+    $scope.$apply(function(){
+      $scope.pages=new Array(pages);
+    });
+  }
+
+  function getAndRenderIssues() {
     getStandardQuery().
       exec().
       then(renderData);
@@ -91,16 +91,11 @@ function IssuesListController ($http, $scope, $routeParams, db,
     return Promise.resolve(db);
   }
 
-  function subscribeToIssues(db) {
-    var predicate = lovefieldQueryBuilder({
-      repository: $routeParams.repo,
-      organization: $routeParams.org
-    });
+  function subscribeToIssues() {
     observeQuery = db.select().from(table).where(predicate)
 
     //Stay abreast of further updates
     db.observe(observeQuery, updateData);
-    return db;
   }
 
   function insertFakeData(db) {
@@ -111,7 +106,90 @@ function IssuesListController ($http, $scope, $routeParams, db,
     return db;
   }
 
+  function showError() {
+    $scope.error = 'Could not update issues from server';
+  }
+
+  function renderData(issues) {
+    $scope.$apply(function() {
+      $scope.issues = issues;
+    })
+  }
+
+  //TODO: eventually utilize observer changes if API can provide what we need
+  function updateData () {
+    getStandardQuery().exec().then(function(issues){
+      console.log('updating issues', issues);
+      $scope.$apply(function() {
+        $scope.issues = issues;
+      });
+    });
+
+    countPages();
+    updateCount();
+  }
+}
+
+function issuesCacheUpdaterFactory($http, $routeParams, firebaseAuth) {
+  var db;
+  var table;
+  var updatedAt
+  return function(_db_) {
+    console.log('starting cache fetch');
+    db = _db_;
+    table = db.getSchema().getIssues();
+    updateIssuesCache(buildUpdateUrl(), true);
+  }
+
+  function updateIssuesCache (url, firstTime) {
+    $http.get(url).
+      then(insertData).
+      then(function(res) {
+        console.log('got response', res.headers());
+        var nextPage = getNextPageUrl(res);
+        if (firstTime && res.data && res.data.length) {
+          updatedAt = res.data[0].updated_at;
+          console.log('set updatedAt', updatedAt);
+        }
+
+        if (nextPage) {
+          updateIssuesCache(nextPage);
+        } else if (updatedAt) {
+          //All done fetching, let's make a note of it.
+          console.log('saving timestamp to localStorage');
+          localStorage.setItem(localKeyBuilder($routeParams.org, $routeParams.repo), updatedAt)
+        } else {
+          console.log('No updated at timestamp :(')
+        }
+      });
+  }
+
+  function localKeyBuilder(owner, repo) {
+    return owner + ':' + repo + ':last_update';
+  }
+
+  function insertData(res) {
+    var issuesInsert = res.data.map(function(issue) {
+      if (issue.id === 55113588) {
+        console.log('here is the offender', issue);
+      }
+      return table.createRow(issueStorageTranslator(issue));
+    });
+    console.log('start insertOrReplace');
+    db.insertOrReplace().into(table).values(issuesInsert).exec().then(function() {
+      console.log('done inserting');
+    }, function(e) {
+      console.log('problem inserting', e.stack);
+    });
+    console.log('end insertOrReplace');
+    return res;
+  }
+
   function buildUpdateUrl() {
+    //TODO: order by and updated since
+    //TODO: store timestamp in localstorage
+    var lastUpdated = localStorage.getItem(
+        localKeyBuilder($routeParams.org, $routeParams.repo));
     return 'https://api.github.com/repos/'+
       $routeParams.org+
       '/'+
@@ -119,28 +197,22 @@ function IssuesListController ($http, $scope, $routeParams, db,
       '/issues?'+
       'per_page=100&'+
       'state=all&'+
+      'sort=updated&'+
+      (lastUpdated?'since='+lastUpdated+'&':'')+
       'access_token='+
       firebaseAuth.getAuth().github.accessToken;
   }
 
-  function updateIssuesCache(url) {
-    $http.get(url).
-      then(insertData).
-      then(function(res) {
-        var nextPage = getNextPageUrl(res);
-        if (nextPage) updateIssuesCache(nextPage);
-      }).
-      then(null, showError);
-  }
-
-  function insertData(res) {
-    var issuesInsert = res.data.map(function(issue) {
-      return table.createRow(issueStorageTranslator(issue));
-    });
-    db.insertOrReplace().into(table).values(issuesInsert).exec();
-
-    return res;
-  }
+  function issueStorageTranslator(issue){
+    /*jshint camelcase: false */
+    var newIssue = angular.copy(issue);
+    newIssue.assignee = issue.assignee || -1;
+    newIssue.milestone = issue.milestone || -1;
+    newIssue.created_at = new Date(issue.created_at);
+    newIssue.updated_at = new Date(issue.updated_at);
+    newIssue.closed_at = new Date(issue.closed_at);
+    return newIssue;
+  };
 
   function getNextPageUrl (res) {
     var linkHeader = res.headers('link')
@@ -152,41 +224,17 @@ function IssuesListController ($http, $scope, $routeParams, db,
       return firstLinkTuple[0].replace(/[<>]*/g, '');
     }
   }
-
-  function showError() {
-    $scope.error = 'Could not update issues from server';
-  }
-
-  function renderData(issues) {
-    $scope.$apply(function() {
-      $scope.issues = issues;
-    })
-    console.log('total query plus render time: ', performance.now() - queryStartTime)
-  }
-
-  //TODO: make this more efficient
-  function updateData (changes) {
-    $scope.$apply(function() {
-      $scope.issues = changes[0].object;
-    })
-  }
-
-
-  function issueStorageTranslator(issue){
-    /*jshint camelcase: false */
-    var newIssue = angular.copy(issue);
-    newIssue.assignee = issue.assignee || -1;
-    newIssue.milestone = issue.milestone || -1;
-    newIssue.created_at = new Date(issue.created_at);
-    newIssue.updated_at = new Date(issue.updated_at);
-    return newIssue;
-  };
 }
 
 angular.module('ghIssuesApp').
+  factory(
+      'issuesCacheUpdater',
+      ['$http', '$routeParams', 'firebaseAuth',
+          issuesCacheUpdaterFactory]).
   controller(
       'IssuesListController',
-      ['$http', '$scope', '$routeParams', 'db', 'lovefieldQueryBuilder',
-          'mockIssues', 'firebaseAuth', IssuesListController]);
+      ['$http', '$scope', '$routeParams', 'db', 'issuesCacheUpdater',
+          'lovefieldQueryBuilder', 'mockIssues', 'firebaseAuth',
+          IssuesListController]);
 
 }());
