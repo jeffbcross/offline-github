731f359d87c5d9e3a5ff06bbfa13a6e3806d87ae
Jeff Cross <middlefloor@gmail.com>
2015-01-22 14:40:08 -0800

    WIP: insert users and milestones into db from issue

--- client/org/repo/issues/issues-list-controller.js
+++ client/org/repo/issues/issues-list-controller.js
@@ -191,16 +191,30 @@ function issuesCacheUpdaterFactory($http, $routeParams, firebaseAuth) {
     totalAdded += (res && res.data && res.data.length) || 0;
     console.log('adding more data', totalAdded);
     console.log('res', res);
-    var issuesInsert = res.data.map(function(issue) {
-      return table.createRow(issueStorageTranslator(issue));
-    });
-    console.log('issuesInsert', issuesInsert)
-    return db.insertOrReplace().into(table).values(issuesInsert).exec().then(function() {
-      console.log('done inserting');
-      return res;
-    }, function(e) {
-      console.log('problem inserting', e.message);
-    });
+
+    return Promise.all(res.data.map(function(issue) {
+      console.log('mapping', issue);
+      var promise = issueStorageTranslator(issue);
+      console.log('then', promise.then);
+      return promise.then(createRow);
+    })).
+      then(function(issues) {
+        console.log('issues');
+        return db.insertOrReplace().into(table).values(issues).exec()
+      }, function(e) {
+        console.log('could not get issues', e.stack)
+      }).
+      then(function() {
+        console.log('done inserting');
+        return res;
+      }, function(e) {
+        console.log('problem inserting', e.message);
+      });
+  }
+
+  function createRow(issue) {
+    console.log('createRow', issue);
+    return table.createRow(issue);
   }
 
   function buildUpdateUrl() {
@@ -224,15 +238,97 @@ function issuesCacheUpdaterFactory($http, $routeParams, firebaseAuth) {
 
   function issueStorageTranslator(issue){
     /*jshint camelcase: false */
-    var newIssue = angular.copy(issue);
-    newIssue.assignee = issue.assignee || -1;
-    newIssue.body = issue.body || '';
-    newIssue.milestone = issue.milestone || -1;
-    newIssue.created_at = new Date(issue.created_at);
-    newIssue.updated_at = new Date(issue.updated_at);
-    newIssue.closed_at = new Date(issue.closed_at);
-    return newIssue;
-  };
+    var usersTable = db.getSchema().getUsers();
+    var milestonesTable = db.getSchema().getMilestones();
+
+    return userStorageTranslator(issue.user).
+      then(function(user) {
+        var row = usersTable.createRow(user);
+        return db.
+          insertOrReplace().
+          into(usersTable).
+          values([row]).
+          exec().
+          then(function() {
+            console.log('then transform milestone');
+            return milestoneStorageTranslator(issue.milestone);
+          }).
+          then(function(milestone) {
+            console.log('then insert milestone');
+            var row = milestonesTable.createRow(milestone);
+            return db.
+              insertOrReplace().
+              into(milestonesTable).
+              values([row]).
+              exec()
+          }).
+          then(function() {
+            console.log('then create issue');
+            var newIssue = angular.copy(issue);
+            newIssue.assignee = issue.assignee || -1;
+            newIssue.body = issue.body || '';
+            newIssue.user = issue.user.id;
+
+            newIssue.milestone = issue.milestone || -1;
+            newIssue.created_at = new Date(issue.created_at);
+            newIssue.updated_at = new Date(issue.updated_at);
+            newIssue.closed_at = new Date(issue.closed_at);
+            return newIssue;
+          });
+      });
+  }
+
+  function userStorageTranslator(user) {
+    var newUser = angular.copy(user);
+    newUser.login =user.login || '';
+    newUser.id =parseInt(user.id, 10);
+    newUser.avatar_url =user.avatar_url || '';
+    newUser.gravatar_id =user.gravatar_id || '';
+    newUser.url =user.url || '';
+    newUser.html_url =user.html_url || '';
+    newUser.followers_url =user.followers_url || '';
+    newUser.following_url =user.following_url || '';
+    newUser.gists_url =user.gists_url || '';
+    newUser.starred_url =user.starred_url || '';
+    newUser.subscriptions_url =user.subscriptions_url || '';
+    newUser.organizations_url =user.organizations_url || '';
+    newUser.repos_url =user.repos_url || '';
+    newUser.events_url =user.events_url || '';
+    newUser.received_events_url =user.received_events_url || '';
+    newUser.type =user.type || '';
+    newUser.site_admin =user.site_admin || false;
+    newUser.name =user.name || '';
+    newUser.company =user.company || '';
+    newUser.blog =user.blog || '';
+    newUser.location =user.location || '';
+    newUser.email =user.email || '';
+    newUser.hireable =user.hireable || false;
+    newUser.bio =user.bio || '';
+    newUser.public_repos =parseInt(user.public_repos, 10) || -1;
+    newUser.public_gists =parseInt(user.public_gists, 10) || -1;
+    newUser.followers =parseInt(user.followers, 10) || -1;
+    newUser.following =parseInt(user.following, 10) || -1;
+    newUser.created_at =new Date(user.created_at);
+    newUser.updated_at =new Date(user.updated_at);
+    return Promise.resolve(newUser);
+  }
+
+  function milestoneStorageTranslator (milestone) {
+    var newMilestone = angular.copy(milestone);
+    newMilestone.url = milestone.url || '';
+    newMilestone.number = parseInt(milestone.number, 10);
+    newMilestone.state = milestone.state || '';
+    newMilestone.title = milestone.title || '';
+    newMilestone.description = milestone.description || '';
+    newMilestone.creator = parseInt(milestone.creator, 10);
+    newMilestone.open_issues = parseInt(milestone.open_issues, 10);
+    newMilestone.closed_issues = parseInt(milestone.closed_issues, 10);
+    newMilestone.created_at = new Date(milestone.created_at);
+    newMilestone.updated_at = new Date(milestone.updated_at);
+    newMilestone.closed_at = new Date(milestone.closed_at);
+    newMilestone.due_on = new Date(milestone.due_on);
+    return Promise.resolve(newMilestone);
+  }
 
   function getNextPageUrl (res) {
     var linkHeader = res && res.headers('link')
